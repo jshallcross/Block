ef03caf0ebbcab17c434ee73ecbca9255fddc2ce
Jon Shallcross <jshallcross1@icloud.com>
2020-06-05 15:42:50 -0400

    commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+          app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+
+
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
+    console.log("SERVER HAS STARTED");
+});
+
+
